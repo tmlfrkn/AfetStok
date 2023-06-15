a0fa4bee0125cb31c4771f071b16627297418bad
Furkan <furkan@Furkan-MacBook-Pro-2.local>
2023-06-15 15:12:12 +0300

    deneme

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React from "react";
 import './App.css';
 import Login from "./components/Login";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import SignedIn from "./components/SignedIn"; // Geçici
+import SignedIn from "./components/SignedIn"; 
 import Dashboard from "./components/Dashboard";
 import AboutUs from "./components/AboutUs";
 import FirebaseInitializer from "./components/dbsetup";
 
 function App() {
 
-  
+  // <FirebaseI../>
   return (
 
 
     <div>
-      <FirebaseInitializer/>
+
       <Routes>
         <Route path="/signed-in" element={<SignedIn />} />
         <Route path="/login" element={<Login />} />
